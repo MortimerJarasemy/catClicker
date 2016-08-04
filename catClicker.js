95373a0d0d05ed2011447a17aeba00b6668a29fa
Mortimer Jarasemy <marcoscolladosegura@yandex.com>
2016-08-04 11:33:10 +0200

    it works

--- catClicker.js
+++ catClicker.js
@@ -1,10 +1,10 @@
 
 $(document).ready(function clickCount(){
-	var a = '0';
+	var a = 0;
 	document.getElementById("myClicks").value = a;
       myCat = document.getElementById('clickMe');
       $('#clickMe').on('click', function(event){
-        a = +1;
+        a = a+1;
 		document.getElementById("myClicks").value = a;
     });
  });
