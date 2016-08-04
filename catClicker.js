929a773282bccff9e7bf7f50a35543fff81de388
Mortimer Jarasemy <marcoscolladosegura@yandex.com>
2016-08-04 11:48:31 +0200

    chindasvinto joined party

--- catClicker.js
+++ catClicker.js
@@ -1,10 +1,20 @@
 
 $(document).ready(function clickCount(){
 	var a = 0;
-	document.getElementById("myClicks").value = a;
+	document.getElementById('myClicks').value = a;
       myCat = document.getElementById('clickMe');
       $('#clickMe').on('click', function(event){
         a = a+1;
 		document.getElementById("myClicks").value = a;
     });
  });
+
+ $(document).ready(function clickCount(){
+ 	var a = 0;
+ 	document.getElementById('myClicks1').value = a;
+       myCat = document.getElementById('clickMe');
+       $('#clickMe1').on('click', function(event){
+         a = a+1;
+ 		document.getElementById("myClicks1").value = a;
+     });
+  });
